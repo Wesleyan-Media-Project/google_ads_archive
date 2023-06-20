a4293e1839ca73704118feaedd13f680438da8a1
Pavel <poleinikov@wesleyan.edu>
2023-06-20 08:43:02 -0400

    Update daily_delta_g_creatives.sql

--- daily_delta_g_creatives.sql
+++ daily_delta_g_creatives.sql
@@ -1,8 +1,51 @@
-insert into `wmp-project.g_ads_targeting.creative_stats_delta` 
-    with a as (select * from `bigquery-public-data.google_political_ads.creative_stats` 
+-- query that will be saved as scheduled query "import_creatives_delta"
+insert into `intro-sql-workshop.my_ad_archive.google_creative_delta` 
+    with a as (select 
+        ad_id, 
+        ad_url, 
+        ad_type, 
+        regions, 
+        advertiser_id, 
+        advertiser_name, 
+        ad_campaigns_list, 
+        date_range_start, 
+        date_range_end, 
+        num_of_days, 
+        impressions, 
+        spend_usd, 
+        first_served_timestamp, 
+        last_served_timestamp, 
+        age_targeting, 
+        gender_targeting, 
+        geo_targeting_included, 
+        geo_targeting_excluded, 
+        spend_range_min_usd, 
+        spend_range_max_usd
+     from `bigquery-public-data.google_political_ads.creative_stats` 
         EXCEPT DISTINCT 
-        select * EXCEPT(import_date, import_time) 
-        from `wmp-laura.g_ads_targeting.creative_stats_delta`) 
-    select a.* except (spend_range_min_brl, spend_range_max_brl), 
+        select 
+            ad_id, 
+            ad_url, 
+            ad_type, 
+            regions, 
+            advertiser_id, 
+            advertiser_name, 
+            ad_campaigns_list, 
+            date_range_start, 
+            date_range_end, 
+            num_of_days, 
+            impressions, 
+            spend_usd, 
+            first_served_timestamp, 
+            last_served_timestamp, 
+            age_targeting, 
+            gender_targeting, 
+            geo_targeting_included, 
+            geo_targeting_excluded, 
+            spend_range_min_usd, 
+            spend_range_max_usd
+
+        from `intro-sql-workshop.my_ad_archive.google_creative_delta`) 
+    select a.*,  
         CAST(@run_date as DATE) as import_date, CAST(@run_time as TIMESTAMP) as import_time, 
-        a.spend_range_min_brl, spend_range_max_brl from a;
+        from a;
