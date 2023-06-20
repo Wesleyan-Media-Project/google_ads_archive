e10c8e51f42f5588ee7d795c6ebb34884e405f37
Pavel <poleinikov@wesleyan.edu>
2023-06-20 08:42:14 -0400

    Update add_g_advertiser_spend.sql

--- add_g_advertiser_spend.sql
+++ add_g_advertiser_spend.sql
@@ -1,12 +1,15 @@
 -- this is a scheduled query add_g_advertiser_spend
 -- it runs once a day and inserts a snapshot of the whole archive
 -- into the table that WMP owns
-insert into g_ads_targeting.my_advertiser_stats 
-    select advertiser_id, advertiser_name, public_ids_list, regions, elections, 
-        total_creatives, spend_usd, spend_eur, 
-        SAFE_CAST(spend_inr AS STRING) as spend_inr, 
-        spend_bgn, spend_hrk, spend_czk, spend_dkk, 
-        SAFE_CAST(spend_huf AS STRING), 
-        spend_pln, spend_ron, spend_sek, spend_gbp, spend_nzd, 
-        CAST(@run_date as DATE) as import_date 
+insert into intro-sql-workshop.my_ad_archive.google_advertiser_agg_spend
+    select advertiser_id, 
+        advertiser_name, 
+        public_ids_list, 
+        regions, 
+        elections, 
+        total_creatives, 
+        spend_usd,
+        CAST(@run_date as DATE) as import_date,
+        CAST(@run_time as TIMESTAMP) as import_time 
         from `bigquery-public-data.google_political_ads.advertiser_stats`;
+    
