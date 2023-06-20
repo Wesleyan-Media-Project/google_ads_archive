60bc7c091c174fa08f16cf159fbfa7f8963eb7e5
Pavel <poleinikov@wesleyan.edu>
2023-06-20 08:57:37 -0400

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,4 +1,4 @@
-create table intro-sql-workshop.my_ad_archive.google_advertiser_agg_spend
+create or replace table intro-sql-workshop.my_ad_archive.google_advertiser_agg_spend
 (
     advertiser_id STRING, 
     advertiser_name STRING, 
@@ -10,7 +10,7 @@ create table intro-sql-workshop.my_ad_archive.google_advertiser_agg_spend
     import_date DATE,
     import_time TIMESTAMP);
 
-create table intro-sql-workshop.my_ad_archive.google_creative_delta
+create or replace table intro-sql-workshop.my_ad_archive.google_creative_delta
 (
     ad_id STRING, 
     ad_url STRING, 
